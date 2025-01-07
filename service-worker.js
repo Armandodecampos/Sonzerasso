cdac9d985f4b1fba14cb695e687a770b33165710
Armandodecampos <161892677+Armandodecampos@users.noreply.github.com>
2025-01-07 20:26:49 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,18 @@
 self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado.');
-  event.waitUntil(self.skipWaiting());
+    console.log('Service Worker instalado.');
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker ativado.');
-  event.waitUntil(self.clients.claim());
+    console.log('Service Worker ativado.');
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Interceptando fetch:', event.request.url);
-  
-  if (event.request.url.match(/\.(mp3|wav)$/)) {
-    return;
-  }
-  
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    console.log('Interceptando fetch:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
